adcc4f3931378499aed8b455bfacd4bdfd765dac
RyanLarge13 <ryanlarge13@gmail.com>
2023-12-23 17:02:36 -0500

    Content Creation, Style Update
    Content Creation && Styles:
    
    Updating a few global styles for the markup section of my blog.
    Also a new style in formatting the way the blog page looks and lays out blog articles

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -12,44 +12,38 @@ const page = async () => {
   const posts = await getPosts();
 
   return (
-    <main className="pt-40 px-10 lg:px-40">
-      <h1 className="text-2xl mb-3">Worst Reads</h1>
-      <section className="grid grid-cols-1 place-items-center md:grid-cols-2 lg:grid-cols-3 gap-10 pb-10">
-        {posts.map((post) => (
-          <div
-            key={post.id}
-            className="bg-[#333] rounded-md shadow-md p-5 duration-200 hover:translate-y-2 cursor-pointer relative w-full h-full"
-          >
-            <a href={`/blog/post/${post.id}`}>
+    // <main className="pt-40 px-10 lg:px-40">
+    <main>
+      <section className="grid grid-cols-1 md:grid-cols-2">
+        {posts.map((post, index) => (
+          <a key={post.id} href={`/blog/post/${post.id}`}>
+            <div
+              className={`bg-[#333] cursor-pointer relative w-full h-full flex flex-col md:flex-row justify-center items-center duration-200 hover:scale-95 ${
+                index % 4 === 0 || (index - 1) % 4 === 0
+                  ? "flex-row-reverse"
+                  : ""
+              }`}
+            >
               {post.featured && (
                 <div className="absolute top-1 left-1 text-yellow-300">
                   <GrStar />
                 </div>
               )}
               <img
-                className="w-full rounded-sm h-[150px] object-cover"
+                className="w-full md:w-[50%] rounded-sm h-[50vh] object-cover"
                 src={post.headerImg}
                 alt="header"
               />
-              <div className="flex justify-between items-start mt-2">
-                <h2 className="text-lg font-semibold border-b border-b-slate-400">
+              <div className="flex flex-col justify-center items-center px-10 my-5 md:my-0">
+                <h2 className="text-4xl text-center border-b-slate-400">
                   {post.title}
                 </h2>
-                <p className="text-sm">
-                  {new Date(post.createdAt).toLocaleDateString("en-US", {
-                    month: "short",
-                    day: "numeric",
-                    year: "numeric",
-                  })}
+                <p className="mt-5 text-sm text-center md:text-left">
+                  {post.desc}
                 </p>
               </div>
-              <p className="mt-1">{post.desc}</p>
-              <div className="flex justify-between items-center mt-4 text-sm">
-                <p>Likes {post.likes}</p>
-                <GrLike />
-              </div>
-            </a>
-          </div>
+            </div>
+          </a>
         ))}
       </section>
     </main>
