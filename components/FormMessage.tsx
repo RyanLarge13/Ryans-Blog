e8a0044d44b05e56be23fb2479e32f8b06fcc4ee
= <=>
2024-07-24 09:11:34 -0400

    Type Errors

--- components/FormMessage.tsx
+++ components/FormMessage.tsx
@@ -1,15 +1,16 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useFormState } from "react-dom";
 
-const initialState = {
- message: ""
+type PropTypes = {
+  serverFunc: () => void;
 };
 
-const FormMessage = ({ serverFunc }) => {
- const [state, formAction] = useFormState(serverFunc, initialState);
+const FormMessage = ({ serverFunc }: PropTypes) => {
+  const [state, setState] = useState<{ message: string }>({ message: "" });
+  const [formAction] = useFormState(serverFunc, void 0);
 
- return <p>{state?.message}</p>;
+  return <p>{state?.message}</p>;
 };
 
 export default FormMessage;
